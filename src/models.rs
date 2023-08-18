c0efe7664c08de5ee09fd128c069506659d1c4a5
Filip Jovanov <filip.jovanov@students.finki.ukim.mk>
2023-08-19 00:06:42 +0200

    Add routes for each base page

--- src/models.rs
+++ src/models.rs
@@ -1,34 +1,54 @@
 use uuid::Uuid;
-use serde::{Serialize, Deserialize};
-use chrono::{Utc, DateTime};
+use serde::*;
+use serde_repr::*;
+use time::PrimitiveDateTime;
 use std::fmt;
+use crate::constants::DATE_TIME_FORMAT;
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+time::serde::format_description!(standard_format, PrimitiveDateTime, DATE_TIME_FORMAT);
+
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize_repr)]
+#[repr(u8)]
 pub enum TicketStatus {
-    Bought
+    Bought = 0,
+    Available = 1
 }
 
 impl fmt::Display for TicketStatus {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             TicketStatus::Bought => write!(f, "Bought"),
+            TicketStatus::Available => write!(f, "Available"),
         }
     }
 }
 
+
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ViewSlot {
+    #[serde(alias = "movieName")]
     pub movie_name: String,
-    pub time_slot: DateTime<Utc> 
+    
+    #[serde(alias = "timeSlot", with="standard_format")]
+    pub time_slot: PrimitiveDateTime
 }
 
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Ticket {
+    #[serde(alias = "guid")]
     pub id: Uuid,
+    
+    #[serde(alias = "seatNumber")]
     pub seat_number: String,
+    
     pub price: f32,
+    
+    #[serde(alias = "viewSlot")]
     pub view_slot: ViewSlot,
+
+    #[serde(alias = "ticketStatus")]
     pub ticket_status: TicketStatus
 }
 
--- src/pages/tickets.rs
+++ src/pages/tickets.rs
@@ -1,40 +1,35 @@
 use leptos::*;
 
-use chrono::prelude::*;
+use crate::models::Ticket;
 
-use uuid::Uuid;
-use crate::models::{Ticket, TicketStatus, ViewSlot};
-
-async fn get_data() -> Vec<Ticket> {
+// TODO: Add proper error handling with status_code checks and custom errors (probably)
+async fn get_data() -> reqwest::Result<Vec<Ticket>> {
     // Make this the official return after getting some data in the database
-    let _data = reqwest::get("https://localhost:44316/api/ticket")
-        .await.unwrap()
-        .json::<Vec<Ticket>>()
-        .await.unwrap();
+    let data = reqwest::get("https://localhost:44316/api/ticket")
+        .await?;
     
-    vec![
-        Ticket {
-            id: Uuid::new_v4(),
-            seat_number: "A11".to_string(),
-            price: 4.32,
-            view_slot: ViewSlot {
-                movie_name: "Test name".to_string(),
-                time_slot: Utc::now(),
-            },
-            ticket_status: TicketStatus::Bought,
-        }
-    ]
+    if !data.status().is_success() {
+        leptos::log!("Passed the get...");
+        leptos::log!("Status: {:?}", data.status());
+        return Ok(Vec::new()); // TODO: Add proper error return so it can be handled down the line
+    }
+
+   // Vec::new().iter().enumerate().collect(); 
+
+    data
+        .json::<Vec<Ticket>>()
+        .await
 }
 
 #[component]
-pub fn TicketItem(cx: Scope, record: Ticket) -> impl IntoView {
+pub fn TicketItem(cx: Scope, idx: usize, record: Ticket) -> impl IntoView {
     view! {
         cx,
         <tr>
-            <th scope="row">1</th>
+            <th scope="row">{idx + 1}</th>
             <td>{record.seat_number}</td>
             <td>{record.price} $</td>
-            <td>{record.view_slot.movie_name} - {record.view_slot.time_slot.format("%Y-%m-%d %H:%M:%S").to_string()}</td>
+            <td>{record.view_slot.movie_name} - {record.view_slot.time_slot.to_string()}</td>
             <td>{format!("{}", record.ticket_status)}</td>
         </tr>
     }
@@ -47,23 +42,35 @@ pub fn TicketItem(cx: Scope, record: Ticket) -> impl IntoView {
 pub fn TicketsPage(cx: Scope) -> impl IntoView {
     // TODO: Add button to swap to "create" page
     // TODO: Add table of ViewSlots, each with their own associated action
-    let resource: leptos::Resource<(), Vec<Ticket>> = create_resource(cx, || (), |_| async move { get_data().await });
+    let resource: leptos::Resource<(), Vec<Ticket>> = create_resource(cx, || (), 
+        |_| async move {
+            match get_data().await {
+                Ok(data) => data,
+                Err(err) => {
+                    leptos::log!("[Tickets Page] Error doing a request to fetch tickets: {:?}", err);
+                    Vec::<Ticket>::new()
+                }
+            }
+        });
    
     // So signals are essentially like useState in React
-    let (data, set_data) = create_signal(cx, Vec::<Ticket>::new());
+    let (data, set_data) = create_signal(cx, Vec::<(usize, Ticket)>::new());
 
     let tickets_data_table = move || {
         let value = resource.read(cx);
         match value {
             None => set_data.set(Vec::new()),
-            Some(val) => set_data.set(val)
+            Some(val) => set_data.set(val.iter()
+                .enumerate()
+                .map(|(idx, record)| (idx, record.clone()))
+                .collect())
         };
 
         view! {cx,
             <For 
                 each = move || data.get()
-                key = |record: &Ticket| record.id
-                view = move |cx, record: Ticket| { view! { cx, <TicketItem record/> } }
+                key = |(_, record)| record.id
+                view = move |cx, record| { view! { cx, <TicketItem idx={record.0} record={record.1}/> } }
             />    
         }
     };
