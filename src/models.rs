5cf67e5eea9fe210d0c7838eaf033d2c5d7c30ac
Filip Jovanov <filip.jovanov@students.finki.ukim.mk>
2023-08-20 06:45:16 +0200

    Finish Tickets Index page (export missing, TBA)

--- src/models.rs
+++ src/models.rs
@@ -1,18 +1,17 @@
 use uuid::Uuid;
 use serde::*;
 use serde_repr::*;
-use time::PrimitiveDateTime;
+use time::{PrimitiveDateTime, macros::format_description, Date};
 use std::fmt;
-use crate::constants::DATE_TIME_FORMAT;
+use crate::constants::*;
 
 time::serde::format_description!(standard_format, PrimitiveDateTime, DATE_TIME_FORMAT);
-
 
 #[derive(Clone, Debug, PartialEq, Serialize_repr, Deserialize_repr)]
 #[repr(u8)]
 pub enum TicketStatus {
-    Bought = 0,
-    Available = 1
+    Available = 0,
+    Bought = 1,
 }
 
 impl fmt::Display for TicketStatus {
@@ -52,4 +51,37 @@ pub struct Ticket {
     pub ticket_status: TicketStatus    
 }
 
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct FilterTickets {
+    #[serde(rename(serialize = "fromTimeSlot"), with = "standard_format::option")]
+    pub from_date: Option<PrimitiveDateTime>,
+
+    #[serde(rename(serialize = "toTimeSlot"), with = "standard_format::option")]
+    pub to_date: Option<PrimitiveDateTime>
+}
+
+impl FilterTickets {
+
+    pub fn new(from: String, to: String) -> FilterTickets {
+        let format = format_description!("[year]-[month]-[day]T[hour]:[minute]");
+        let mut filter = FilterTickets {
+            from_date: None,
+            to_date: None
+        };
+
+        if !from.is_empty() {
+            if let Ok(date) = PrimitiveDateTime::parse(&from, &format) {
+                filter.from_date = Some(date);
+            }
+        }
+
+        if !to.is_empty() {
+            if let Ok(date) = PrimitiveDateTime::parse(&to, &format) {
+                filter.to_date = Some(date);
+            }
+        }
+
+        filter
+    }
+}
 
